68add31c09f8f6eb5f83bc8ce8850d6541d4efee
Alexandra Stanford <37681368+alxthedesigner@users.noreply.github.com>
2021-07-13 16:25:49 -0500

    From python to Flask

--- word_number_app.py
+++ word_number_app.py
@@ -4,25 +4,46 @@ from flask import request
 application = Flask(__name__)   #App Instance created
 
 @application.route("/")         #When URL ends in '/', execute method
-num1 = input("Enter random number:")
+  return WORDHTML
 
-def doubleNumber(num):    #Create 'doubleNumber' method
-  doubled = num*2
-  print "The doubled value of", num, "is", doubled
+  WORDHTML = """
+     <html><body>
+         <h2>Word and Number Game</h2>
+         <form action="/game">
+             Enter a word: <input type='text' name='wrd'><br>
+             Enter a number <input type='text' name='num'><br>
+             <input type='submit' value='Continue'>
+         </form>
+     </body></html>"""
 
-@application.route("/word")
-word1 = raw_input("Enter a word: ")
-       
-def wordLength(wrd):
-    wordL = len(wrd)
-    print "Your word has", wordL, "letters"
+  
+  
+@application.route("/game")
+  word = request.args.get('wrd', '')
+  number = request.args.get('num', '')
+  
+  
+def infoDescription():
+    doubled = number*2
+    #print "The doubled value of", num, "is", doubled
+  
+    wordL = len(word)
+    wordmsg1 = "Your word has", wordL, "letters"
 
-def spellBackwards(wrd):
-    backwrd = wrd[::-1]
-    if wrd == backwrd:
-        print "Your word is a palindrome!"
-
-    print "Your word spelled backwards is", backwrd
+    backwrd = word[::-1]
+    if word == backwrd:
+        wordmsg2 = "Your word is a palindrome! Your word spelled backwards is", backwrd
+    else:
+        wordmsg2 = "Your word spelled backwards is", backwrd
+    
+    return DESCRIPTIONHTML.format(doubled, wordmsg1, wordmsg2)
+  
+  DESCRIPTIONHTML = """
+     <html><body>
+         <h2>Your number doubled is, {0}!</h2>
+         {1}, {2}
+     </body></html>
+     """
 
 if __name__ == "__main__":
     application.run()             # run flask App Instance
