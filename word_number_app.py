6358f93a02c362992ac8dc3d5a3cb0e140ce9ffa
Alexandra Stanford <37681368+alxthedesigner@users.noreply.github.com>
2021-07-13 16:36:43 -0500

    Formatting

--- word_number_app.py
+++ word_number_app.py
@@ -4,12 +4,12 @@ from flask import request
 application = Flask(__name__)   #App Instance created
 
 @application.route("/")         #When URL ends in '/', execute method
-   def home():
-      return WORDHTML
+def home():
+   return WORDHTML
 
-  WORDHTML = """
-     <html><body>
-         <h2>Word and Number Game</h2>
+WORDHTML = """
+   <html><body>
+      <h2>Word and Number Game</h2>
          <form action="/game">
              Enter a word: <input type='text' name='wrd'><br>
              Enter a number <input type='text' name='num'><br>
@@ -19,31 +19,31 @@ application = Flask(__name__)   #App Instance created
 
   
   
-@application.route("/game")
-  word = request.args.get('wrd', '')
-  number = request.args.get('num', '')
-  
-  def infoDescription():
-      doubled = number*2
-      #print "The doubled value of", num, "is", doubled
+@application.route("/game") 
+def infoDescription():
+   word = request.args.get('wrd', '')
+   number = request.args.get('num', '')
+   doubled = number*2
+   #print "The doubled value of", num, "is", doubled
 
-      wordL = len(word)
-      wordmsg1 = "Your word has", wordL, "letters"
+   wordL = len(word)
+   wordmsg1 = "Your word has", wordL, "letters"
 
-      backwrd = word[::-1]
-      if word == backwrd:
-          wordmsg2 = "Your word is a palindrome! Your word spelled backwards is", backwrd
-      else:
-          wordmsg2 = "Your word spelled backwards is", backwrd
+   backwrd = word[::-1]
+   if word == backwrd:
+      wordmsg2 = "Your word is a palindrome! Your word spelled backwards is", backwrd
+   else:
+      wordmsg2 = "Your word spelled backwards is", backwrd
 
-      return DESCRIPTIONHTML.format(doubled, wordmsg1, wordmsg2)
-  
-  DESCRIPTIONHTML = """
-     <html><body>
-         <h2>Your number doubled is, {0}!</h2>
+   return DESCRIPTIONHTML.format(doubled, wordmsg1, wordmsg2)
+
+
+DESCRIPTIONHTML = """
+   <html><body>
+      <h2>Your number doubled is, {0}!</h2>
          {1}, {2}
-     </body></html>
-     """
+   </body></html>
+   """
 
 if __name__ == "__main__":
     application.run()             # run flask App Instance
